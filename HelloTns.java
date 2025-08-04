e58bee0d78f213a887ed1cc6fec69d5e5d4891c4
GANGADHARI VIVEK <gangadharivivek6@gmail.com>
2025-08-04 21:46:22 +0530

    Add files via upload
    intiall commit

---
+++ HelloTns.java
@@ -0,0 +1,9 @@
+public class HelloTns {
+    public void print() {
+        System.out.println("welcome to the  tns program");
+    }
+    public static void main(String[] args) {
+        HelloTns obj1 = new HelloTns(); 
+        obj1.print(); 
+    }
+}
